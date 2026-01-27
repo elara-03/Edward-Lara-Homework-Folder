38cd46467e051f39299b381ea004846270ef1905
Edward Lara <edwardlara@cpp.edu>
2026-01-26 21:10:43 -0800

    new file for HW

--- helpme.cpp
+++ helpme.cpp
@@ -1,1 +1,8 @@
-coding fun
+#include <iostream> // allows use of std:: cout
+int main() 
+ { std:: cout<< "Hello world";
+// output text to the console 
+return 0;
+//indicate successful program end
+}
+
