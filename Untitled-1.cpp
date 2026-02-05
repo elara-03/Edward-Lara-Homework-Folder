a52cc71310856bd8c7aa2e9244daddcb27d8d085
Edward Lara <edwardlara@cpp.edu>
2026-02-04 21:29:32 -0800

    Homework 1 commit

---
+++ Untitled-1.cpp
@@ -0,0 +1,4 @@
+#include <iostream> // allows use of std:: cout
+int main() // program entry point { std:: cout<< "Hello World";
+// output text to the console 
+return 0; //indicate successful program end}
--- helpme.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream> // allows use of std:: cout
-int main() 
- { std:: cout<< "Hello world";
-// output text to the console 
-return 0;
-//indicate successful program end
-}
-
