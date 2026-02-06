caa12151765e38e69bb3ab01565848b5aedcef30
Edward Lara <edwardlara@cpp.edu>
2026-02-05 23:38:31 -0800

    Homework 02

---
+++ Homework/Homework 02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){string country;
+cout << "where do you live?" << endl;
+cin >> country; 
+
+cout << "Oh cool I've heard great things about" << country;
+cout << "I've always wanted to go there" << endl;
+return 0;
+}
