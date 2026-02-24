9c5e7f7d75de741f7395f314661cb1e6ea8854c9
Edward Lara <edwardlara@cpp.edu>
2026-02-23 23:43:56 -0800

    final commmit hw5

--- Homework/Homework 05.cpp
+++ Homework/Homework 05.cpp
@@ -2,7 +2,17 @@
 int main(void) {
 char message[1024];
 int vowel_count = 0;
-printf("Input message here");
+printf("Input message here:");
 if (fgets(message, sizeof(message), stdin) == NULL)
 {return 1;}
+
+for (int i = 0; message[i] != '\0'; i++)
+{ char c = message[i]; 
+if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
+c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') 
+{ vowel_count++;}
+}
+
+printf("Number of vowels: %d\n", vowel_count);
+return 0;
 }
