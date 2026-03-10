4becfb11797217499a4e451558bb9645a8a8c6c8
Edward Lara <edwardlara@cpp.edu>
2026-03-09 17:25:22 -0700

    part 1

--- Homework/Homework 06.cpp
+++ Homework/Homework 06.cpp
@@ -1,1 +1,26 @@
-// Ill do hw 6 tmrw
+#include <iostream>
+#include <array>
+
+int main() {
+    int data[] {1,3,6,7,9};
+    int data1[] {5,3,3,11};
+
+size_t size = std::size(data1); 
+
+for(size_t i = 0; i < size; i++) {
+    int temp = data[i];
+    data[i] = data1[i];
+    data1[i] = temp;
+}
+
+std::cout << "data: ";
+for(size_t i = 0; i < std::size(data); i++) {
+std::cout << data[i] << " ";
+}
+
+std::cout << "\ndata1: ";
+for(size_t i = 0; i < std::size(data1); i++) {
+std::cout << data1[i] << " ";
+}
+
+}
